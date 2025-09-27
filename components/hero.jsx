522da8c3a287635b5a485c079c712e3d0bfc63f8
Louis <louis.worknproductivity@gmail.com>
2025-09-27 17:16:28 +0700

    Added new className for proper profile picture sizing

--- components/hero.jsx
+++ components/hero.jsx
@@ -64,6 +64,7 @@ export function Hero() {
             <img
               src="/Pasfoto_Louis.jpg"
               alt="Profile Picture"
+              className="h-full w-full"
             />
           </div>
         </div>
